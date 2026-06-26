b08487bc62162a5bce9ffa83a4ea60d9d20df47d
Mikel Briz <mikelbriz@gmail.com>
2026-06-26 18:48:35 -0300

    Add typing effect for job titles in script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 
+const texts = [
+    "Data Scientist",
+    "Machine Learning Engineer",
+    "Mathematician",
+    "Data Analyst"
+];
+
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type(){
+    current = texts[i];
+
+    if(isDeleting){
+        document.querySelector(".typing").textContent =
+            current.substring(0, j--);
+    }else{
+        document.querySelector(".typing").textContent =
+            current.substring(0, j++);
+    }
+
+    if(!isDeleting && j === current.length){
+        isDeleting = true;
+        setTimeout(type, 1200);
+        return;
+    }
+
+    if(isDeleting && j === 0){
+        isDeleting = false;
+        i = (i + 1) % texts.length;
+    }
+
+    setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
