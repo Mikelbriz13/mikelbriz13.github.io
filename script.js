43f7b2c59c8386da78fe8f2a25ec2fc23aa739a3
Mikel Briz <mikelbriz@gmail.com>
2026-06-26 19:04:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,32 +8,31 @@ const texts = [
 
 let i = 0;
 let j = 0;
-let current = "";
 let isDeleting = false;
 
-function type(){
-    current = texts[i];
+function type() {
 
-    if(isDeleting){
-        document.querySelector(".typing").textContent =
-            current.substring(0, j--);
-    }else{
-        document.querySelector(".typing").textContent =
-            current.substring(0, j++);
+    const current = texts[i];
+
+    document.querySelector(".typing").textContent =
+        current.substring(0, j);
+
+    if (!isDeleting) {
+        j++;
+        if (j === current.length) {
+            isDeleting = true;
+            setTimeout(type, 1200);
+            return;
+        }
+    } else {
+        j--;
+        if (j === 0) {
+            isDeleting = false;
+            i = (i + 1) % texts.length;
+        }
     }
 
-    if(!isDeleting && j === current.length){
-        isDeleting = true;
-        setTimeout(type, 1200);
-        return;
-    }
-
-    if(isDeleting && j === 0){
-        isDeleting = false;
-        i = (i + 1) % texts.length;
-    }
-
-    setTimeout(type, isDeleting ? 50 : 100);
+    setTimeout(type, isDeleting ? 60 : 100);
 }
 
 type();
